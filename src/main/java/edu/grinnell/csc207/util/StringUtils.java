f2e14fce7e4971cfa0071ea8b07f8280e01e0c8a
yashmalik <malikyas@grinnell.edu>
2024-10-03 14:43:36 -0500

    Implement the  method.

--- src/main/java/edu/grinnell/csc207/util/StringUtils.java
+++ src/main/java/edu/grinnell/csc207/util/StringUtils.java
@@ -1,9 +1,7 @@
 package edu.grinnell.csc207.util;
 
-import edu.grinnell.csc207.linear.Stack;
 import edu.grinnell.csc207.linear.LinkedStack;
-
-import java.io.PrintWriter;
+import edu.grinnell.csc207.linear.Stack;
 
 /**
  * Assorted utilities for working with strings.
@@ -16,12 +14,37 @@ public class StringUtils {
   // | Provided methods |
   // +------------------+
     
+  private static boolean parensMatch(Character a, Character b) { 
+    if(a.equals('(') && b.equals(')')) return true;
+    if(a.equals('{') && b.equals('}')) return true;
+    if(a.equals('[') && b.equals(']')) return true;
+    return false;
+  }
+
   /**
    * Determine whether the parens match in string.
    */
   public static boolean checkMatching(String str) {
     Stack<Character> parens = new LinkedStack<Character>();
-    return false;       // STUB
+    for(int i = 0; i < str.length(); ++i) { 
+      char c = str.charAt(i);
+      if("{([".indexOf(c) != -1) { 
+        try {
+          parens.put(c);
+        } catch(Exception e) { 
+
+        }
+      } else if("])}".indexOf(c) != -1) { 
+        try {
+          if(!parensMatch(parens.get(), c)) { 
+            return false;
+          }
+        } catch (Exception e) {
+          return false;
+        }
+      }
+    }
+    return parens.isEmpty();
   } // checkMatching
 } // class StringUtils    
 
